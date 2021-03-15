34f60b8abb8b7c3dd1956df6bcf38bf153529f0c
Giusty Fadh Wiranapoera <54473048+Giusty@users.noreply.github.com>
2021-03-15 13:29:37 +0700

    another_type_of_employee

--- APPL_1_W1/another_type_of_employee/Firm.java
+++ APPL_1_W1/another_type_of_employee/Firm.java
@@ -7,12 +7,12 @@ package another_type_of_employee;
 // 2B - D4 Teknik Informatika
 // Politeknik Negeri Bandung
 //********************************************************************
-public class Firm{}
+public class Firm{
 //-----------------------------------------------------------------
 // Creates a staff of employees for a firm and pays them.
 //-----------------------------------------------------------------
-public static void main (String[] args){
-    Staff personnel = new Staff();
-    personnel.payday();
+    public static void main (String[] args){
+        Staff personnel = new Staff();
+        personnel.payday();
     }
 }
